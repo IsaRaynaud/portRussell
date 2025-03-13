2b5cfe0783778cbdc6a78453507fb6ac644f035e
IsaRaynaud <raynaud.isabel@gmail.com>
2025-03-13 13:51:12 +0100

    Add gitignore

--- api/app.js
+++ api/app.js
@@ -2,13 +2,15 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 const indexRouter = require('./routes/index');
-//
+const catwaysRouter = require('./routes/catways');
 
-//const usersRouter = require('./routes/users');
+const app = express();
 
-cont app = express();
+const mongodb = require('./db/mongo');
+mongodb.initClientDbConnection();
 
 app.use(cors({
     exposedHeaders: [Authorization],
@@ -20,10 +22,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-//app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-//app.use('/users', usersRouter);
+app.use('/catways', catwaysRouter);
 
 app.use(function(req, res, next) {
     res.status(404).json({name:'API', version: '1.0', status:404, message: 'Introuvable'});
