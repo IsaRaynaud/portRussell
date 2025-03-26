95714f1d0cfdce8a8adedc6fac01c24c6f3bdd3f
IsaRaynaud <raynaud.isabel@gmail.com>
2025-03-26 14:40:21 +0100

    Add catways managment to admin dashboard

--- api/routes/catways.js
+++ api/routes/catways.js
@@ -1,16 +1,26 @@
 const express = require('express');
 const router = express.Router();
 
+const Catway = require('../models/catway');
 const service = require('../services/catways');
-const { checkJWT, isAdmin, isClient } = require('../middlewares/private');
+const { checkJWT, isAdmin, isClient, isAdminOrClient } = require('../middlewares/private');
 
-router.use(checkJWT, isClient)
+//Route pour la vue catways.ejs
+router.get('/', (req, res, next) => {
+    console.log("Route GET/catways appelée");
+res.render('catways');
+});
 
-router.get('/', service.get);
-router.get('/:catwayNumber', service.getById);
-router.post('/', isAdmin, service.add);
-router.put('/:catwayNumber', isAdmin, service.updateAll);
-router.patch('/:catwayNumber', isAdmin, service.update);
-router.delete('/:catwayNumber', isAdmin, service.delete);
+//Routes API protégées
+router.get('/api', checkJWT, isAdminOrClient, async (req, res) => {
+    const catways = await Catway.find().sort({catwayNumber : 1});
+    res.status(200).json({catways, role : req.user.role});
+  });
+
+router.get('/:catwayNumber', checkJWT, isAdminOrClient, service.getById);
+router.post('/', checkJWT, isAdmin, service.add);
+router.put('/:catwayNumber', checkJWT, isAdmin, service.updateAll);
+router.patch('/:catwayNumber', checkJWT, isAdmin, service.update);
+router.delete('/:catwayNumber', checkJWT, isAdmin, service.delete);
 
 module.exports = router;
